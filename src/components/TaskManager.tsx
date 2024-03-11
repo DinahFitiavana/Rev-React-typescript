c7ff6a817a13426bb6db5bd17b2ef651ef575458
DinahFitiavana <hei.dinah.3@gmail.com>
2024-03-11 08:00:57 +0300

    typescript

--- src/components/TaskManager.tsx
+++ src/components/TaskManager.tsx
@@ -1,7 +1,65 @@
-import { nanoid } from "nanoid";
-import { useState } from "react";
+//import { nanoid } from "nanoid";
+//import { useState } from "react";
 import "./TaskManager.css";
 
+interface Task {
+  id: number;
+  title: string;
+  searchKeyword: string;
+  task: string;
+}
+
+class TaskManager {
+  private tasks: Task[];
+
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask(task: Task): void {
+    this.tasks.push(task);
+  }
+
+  deleteTask(id: number): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  getTask(id: number): Task | undefined {
+    return this.tasks.find(task => task.id === id);
+  }
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+}
+
+// Exemple d'utilisation
+const taskManager = new TaskManager();
+
+const task1: Task = {
+  id: 1,
+  title: "Faire les courses",
+  searchKeyword: "courses, achats",
+  task: "Acheter des légumes et du pain",
+};
+
+const task2: Task = {
+  id: 2,
+  title: "Faire du sport",
+  searchKeyword: "sport, fitness",
+  task: "Courir pendant 30 minutes",
+};
+
+taskManager.addTask(task1);
+taskManager.addTask(task2);
+
+console.log(taskManager.getAllTasks());
+
+
+console.log(taskManager.getAllTasks());
+
+
+/**
 // TODO: create custom hook to manage task state
 export const TaskManager = () => {
   const [title, setTitle] = useState("");
@@ -83,3 +141,4 @@ export const TaskManager = () => {
     </div>
   );
 };
+*/
